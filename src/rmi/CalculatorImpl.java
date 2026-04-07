979a759565a5861453fccffaf672056009271055
Swornim Aryal <Swornim@Swornims-MacBook-Air.local>
2026-04-07 13:09:05 +0530

    rmicodes

---
+++ src/rmi/CalculatorImpl.java
@@ -0,0 +1,35 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
+    protected CalculatorImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public int add(int a, int b) throws RemoteException {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) throws RemoteException {
+        return a - b;
+    }
+
+    @Override
+    public int multiply(int a, int b) throws RemoteException {
+        return a * b;
+    }
+
+    @Override
+    public double divide(int a, int b) throws RemoteException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) a / b;
+    }
+
+}
